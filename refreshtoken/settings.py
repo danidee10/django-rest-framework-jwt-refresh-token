18354b47dd4db6fd9201e0bf63255f6af05b5baf
Osaetin Daniel <osaetindaniel@gmail.com>
2019-01-08 19:22:16 +0100

    Add management command to generate refresh tokens
    - Add new setting 'JWT_APP_NAME' to control the app name of the refresh
    token

---
+++ refreshtoken/settings.py
@@ -0,0 +1,14 @@
+import datetime
+
+from django.conf import settings
+from rest_framework.settings import APISettings
+
+
+USER_SETTINGS = getattr(settings, 'JWT_AUTH', None)
+
+DEFAULTS = {
+    'JWT_APP_NAME':
+    'refreshtoken',
+}
+
+api_settings = APISettings(USER_SETTINGS, DEFAULTS, [])
